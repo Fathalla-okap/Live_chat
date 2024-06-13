5a70eaf531d132eb68b8f6af78c1404aa5ad4e3d
Fathalla-okap <136493772+Fathalla-okap@users.noreply.github.com>
2024-06-14 01:27:13 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const chatBox = document.getElementById('chat-box');
     const messageInput = document.getElementById('message');
     const sendButton = document.getElementById('send-button');
-    const onlineUsersElement = document.getElementById('online-users');
-    const totalVisitorsElement = document.getElementById('total-visitors');
 
     let userId;
 
@@ -14,7 +12,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     socket.on('load_messages', (messages) => {
-        messages.forEach((msg, index) => displayMessage(msg, index));
+        messages.forEach(msg => displayMessage(msg));
     });
 
     sendButton.addEventListener('click', () => {
@@ -26,22 +24,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     socket.on('message', (msg) => {
-        const index = document.getElementsByClassName('message').length;
-        displayMessage(msg, index);
+        displayMessage(msg);
     });
 
-    socket.on('update_online_users', (onlineUsers) => {
-        onlineUsersElement.textContent = `Online Users: ${onlineUsers}`;
-    });
-
-    socket.on('update_total_visitors', (totalVisitors) => {
-        totalVisitorsElement.textContent = `Total Visitors: ${totalVisitors}`;
-    });
-
-    function displayMessage(msg, index) {
+    function displayMessage(msg) {
         const messageElement = document.createElement('div');
         messageElement.className = 'message';
-        messageElement.innerHTML = `<strong>${msg.user}:</strong> ${msg.text}<span class="timestamp">${new Date().toLocaleTimeString()}</span>`;
+        messageElement.innerHTML = `<strong>${msg.user}:</strong> ${msg.text} <span class="timestamp">${msg.timestamp}</span>`;
         chatBox.appendChild(messageElement);
         chatBox.scrollTop = chatBox.scrollHeight;
     }
